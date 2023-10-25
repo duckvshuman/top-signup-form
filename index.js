19df4e3f3bd325b78d61437ada967ab7260a7411
DvH <88995192+DangVietH@users.noreply.github.com>
2023-10-25 13:20:20 +0700

    update style

---
+++ index.js
@@ -0,0 +1,4 @@
+const email = document.getElementById('email')
+const phone = document.getElementById('phone')
+const password = document.getElementById('password')
+const confirmPassword = document.getElementById('confirm-pass')
